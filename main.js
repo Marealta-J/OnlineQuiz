335dd64a0ee40a4d92edb77bdaeaeb8123baa1d9
«Marealta-J» <julia.marealta@gmail.com>
2022-10-05 08:32:04 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const questions = [
+{
+    questions: "The second highest mountain in the worlds?",
+    answers: ["Everest", "Beluga", "K2", "Elbrus"],
+    correct: 3,
+},
+{
+    questions: "The closest star to the earth after the sun?",
+    answers: ["Proxima Centauri", "Sirius A", "Antares", "Betelgeuse"],
+    correct: 1,
+},
+{
+    questions: "What consumes more energy?",
+    answers: ["Quadro RTX 8000", "RTX Titan X", "Radeon VEGA9", "RTX 4070"],
+    correct: 4,
+}
+]
